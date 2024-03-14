1850c79928455df4a31c54dae6f947016fb26d8e
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-03-14 14:47:11 -0700

    updates

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const fs = require("fs");
 const genHTML = require("./lib/genHTML");
 const { questions } = require("./lib/inqQuestions");
 
-// inquirer.prompt(questions).then((ans) => {
-//   fs.writeFile(
-//     `./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,
-//     genHTML(ans),
-//     (err) => {
-//       if (err) {
-//         throw err;
-//       }
-//       console.log("yay1");
-//     }
-//   );
-// });
+inquirer.prompt(questions).then((ans) => {
+  fs.writeFile(
+    `./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,
+    genHTML(ans),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("yay1");
+    }
+  );
+});
 
 
-inquirer.prompt(questions).then((ans) => fs.writeFile(`./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,genHTML(ans),(err) => err?console.log(err):console.log("yay!")));
+//same code one lined just for funzies
+// inquirer.prompt(questions).then((ans) => fs.writeFile(`./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,genHTML(ans),(err) => err?console.log(err):console.log("yay!")));
