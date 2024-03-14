abca937f907c90caa44149f45ea31f249536c022
Joe Rehfuss <jrehfuss@c02g81atq05q.domain>
2024-03-14 14:46:04 -0700

    updates

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const genHTML = require("./lib/genHTML");
+const { questions } = require("./lib/inqQuestions");
+
+// inquirer.prompt(questions).then((ans) => {
+//   fs.writeFile(
+//     `./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,
+//     genHTML(ans),
+//     (err) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log("yay1");
+//     }
+//   );
+// });
+
+
+inquirer.prompt(questions).then((ans) => fs.writeFile(`./output/${ans.user.trim().split(" ").join("-").toLowerCase()}.html`,genHTML(ans),(err) => err?console.log(err):console.log("yay!")));
